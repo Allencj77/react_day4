5854b8b849256a366263209275e54ad20e677c5f
CHRIS ALLEN <727721euec015@skcet.ac.in>
2023-03-31 12:39:22 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import GreetingForm from './Fruit'
+function App() {
+  return (
+    <div className="App">
+      <GreetingForm/>
+    </div>
+  );
+}
+
+export default App;
